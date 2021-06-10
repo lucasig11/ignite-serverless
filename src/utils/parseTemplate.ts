dc2c4c2de4bec19a9255e7ddb6806db88cdf8c6b
lucas <lucasfonsecard@icloud.com>
2021-06-10 09:44:36 -0300

    chore(lint): apply lint in the whole project

--- src/utils/parseTemplate.ts
+++ src/utils/parseTemplate.ts
@@ -1,17 +1,17 @@
-import { readFileSync } from "fs";
-import { compile } from "handlebars";
+import { readFileSync } from 'fs';
+import { compile } from 'handlebars';
 
 interface ITemplateData {
-  path: string;
-  variables: {
-    [key: string]: string | number;
-  };
+    path: string;
+    variables: {
+        [key: string]: string | number;
+    };
 }
 
 export async function parseTemplate({
-  path,
-  variables,
+    path,
+    variables,
 }: ITemplateData): Promise<string> {
-  const html = readFileSync(path, "utf-8");
-  return compile(html)(variables);
+    const html = readFileSync(path, 'utf-8');
+    return compile(html)(variables);
 }
--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,19 +1,19 @@
-import { APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyResult } from 'aws-lambda';
 
 interface IResponse {
-  body: Record<string, unknown>;
-  status?: number;
+    body: Record<string, unknown>;
+    status?: number;
 }
 
 export default function response({
-  body,
-  status = 200,
+    body,
+    status = 200,
 }: IResponse): APIGatewayProxyResult {
-  return {
-    statusCode: status,
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+    return {
+        statusCode: status,
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
 }
