5e067aefd8701179dce6af1404f40e9720e4d1d7
lucas <lucasfonsecard@icloud.com>
2021-06-10 09:31:31 -0300

    feat: add pdf generation

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,6 +1,12 @@
-import {document} from '@utils/dynamoDBClient';
-import {response} from '@utils/response';
-import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import dayjs from "dayjs";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+import { document } from "@utils/dynamoDBClient";
+import { generatePDF } from "@utils/generatePDF";
+import { parseTemplate } from "@utils/parseTemplate";
+import response from "@utils/response";
 
 interface ICreateCertificate {
   id: string;
@@ -9,18 +15,44 @@ interface ICreateCertificate {
 }
 
 export const handle = async (
-    event: APIGatewayProxyEvent,
+  event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const {id, name, grade} = JSON.parse(event.body) as ICreateCertificate;
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  await document.put({
-    TableName: 'users_certificates',
-    Item: {
+  await document
+    .put({
+      TableName: "users_certificates",
+      Item: {
+        id,
+        name,
+        grade,
+      },
+    })
+    .promise();
+
+  const templatePath = join(
+    process.cwd(),
+    "src",
+    "templates",
+    "certificate.hbs"
+  );
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+
+  const medal = readFileSync(medalPath, "base64");
+
+  const content = await parseTemplate({
+    path: templatePath,
+    variables: {
       id,
       name,
       grade,
+      date: dayjs().format("DD/MM/YYYY"),
+      medal,
     },
-  }).promise();
+  });
 
-  return response({body: {message: 'Certificate generated'}, status: 201});
+  await generatePDF(content);
+
+  return response({ body: { message: "Certificate generated" }, status: 201 });
 };
